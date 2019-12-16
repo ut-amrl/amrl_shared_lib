c5c3f3f7a5b1c0f5ce5da4f2a287905a2c471db2
Joydeep Biswas <joydeepb@cs.utexas.edu>
2019-12-16 11:49:33 -0600

    CG issues...

--- math/line2d.h
+++ math/line2d.h
@@ -118,10 +118,8 @@ struct Line {
     // Okay, the line segments definitely intersect.
     const T d = Cross<T>(d2, -d1);
 
-#if !defined(NDEBUG)
-    // Just an extra check, should never happen
     if (d == T(0)) { return false; }
-#endif  // NDEBUG
+
     const T tb = Cross<T>(p0 -p2, p0-p1) / d;
     *intersection = p2 + tb * d2;
     return true;
